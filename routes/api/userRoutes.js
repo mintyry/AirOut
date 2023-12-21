3d988c6fc44dc2533793fdc22f0b1fdd76f52f8c
mintyry <gitmintyry@gmail.com>
2023-12-20 20:40:29 -0800

    successfully made post route, db shows in compass

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,4 +12,18 @@ router.get('/', async (req,res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    console.log(User);
+    console.log('we got the post route');
+    try {
+        console.log('we gonna create');
+        const user = await User.create(req.body);
+        console.log('we in the middle');
+        res.json(user);
+        console.log('we created');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 module.exports = router;
