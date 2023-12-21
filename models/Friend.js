98cb1f96691813ded19d3ba8a159e329c33a6204
mintyry <gitmintyry@gmail.com>
2023-12-20 17:12:40 -0800

    fixed friends property in User

---
+++ models/Friend.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const friendSchema = new mongoose.Schema({
+    
+})
--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const thoughtSchema = require('./Thought');
-const friendSchema = require('./Friend');
+
 
 const userSchema = new mongoose.Schema({
-    username: 
+    username:
     {
         type: String,
         required: true,
@@ -19,7 +19,12 @@ const userSchema = new mongoose.Schema({
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
     },
     thoughts: [thoughtSchema],
-    friends: [friendSchema]
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
 });
 
 //need a virtual
