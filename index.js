f0bc211b3e5ceecd8e2f2866a72ac4388815aa64
mintyry <gitmintyry@gmail.com>
2023-12-20 20:00:25 -0800

    saving

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const db = require('./config/connection')
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const PORT = 3001;
 const app = express(); //allows us to use defined methods to set routes, etc.
 
+//need process.cwd?
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}`);
+    });
+});
