ca2f87efb0305561cf8787164014e072e30992b9
mintyry <gitmintyry@gmail.com>
2023-12-23 19:38:51 -0800

    added comments to user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,8 +5,11 @@ const { User } = require ('../../models');
 
 const { getUsers, getOneUser, createUser, updateUser, deleteUser, addFriend, deleteFriend  } = require('../../controllers/userController');
 
+//handling all users, creating users
 router.route('/').get(getUsers).post(createUser);
+//handling single users
 router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser);
+//handling friends
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
